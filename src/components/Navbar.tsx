15a81b19a4e8718331ab813857c818a887a3c4fb
Susmit Roy <susmit.roy.2006@gmail.com>
2026-05-10 00:14:49 +0530

    Refine navbar and logo spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,48 @@
 import { MenuIcon, XIcon } from "lucide-react";
 import { useState } from "react";
 import { motion } from "motion/react";
-import { navlinks } from "../data/navlinks";
-import type { INavLink } from "../types";
-import { NavLink } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../assests/logo.svg";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
-
+    const navigate = useNavigate();
     return (
         <>
-            <motion.nav className="fixed top-0 z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-32 backdrop-blur"
+            <motion.nav className="fixed top-0 z-50 w-full py-4 px-6 md:px-16 lg:px-24 xl:px-32 backdrop-blur"
                 initial={{ y: -100, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
             >
-                <a href="https://prebuiltui.com?utm_source=pixels">
-                    <img className="h-8.5 w-auto" src="/assets/logo.svg" alt="logo" width={130} height={34} />
-                </a>
+                <div className="mx-auto flex max-w-7xl items-center justify-between gap-10">
+                    <Link to='/' className="shrink-0">
+                        <img src={logo} alt="logo" className="h-28 w-auto"/>
+                    </Link>
 
-                <div className="hidden md:flex items-center gap-8 transition duration-500">
-                    {navlinks.map((link: INavLink) => (
-                        <NavLink key={link.name} to={link.href} className="hover:text-pink-500 transition">
-                            {link.name}
-                        </NavLink>
-                    ))}
+                    <div className="hidden md:flex flex-1 items-center justify-center gap-12 text-lg font-semibold transition duration-500">
+                        <Link to='/' className="hover:text-pink-300 transition">Home</Link>
+                        <Link to='/generate' className="hover:text-pink-300 transition">Generate</Link>
+                        <Link to='/my-generation' className="hover:text-pink-300 transition">My Generations</Link>
+                        <Link to='#' className="hover:text-pink-300 transition">Contact</Link>
+                    </div>
+
+                    <button onClick={()=> navigate('/login')} className="hidden md:block shrink-0 px-6 py-2.5 text-base font-semibold bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
+                        Get Started
+                    </button>
+                    <button onClick={() => setIsOpen(true)} className="md:hidden" aria-label="Open menu" title="Open menu">
+                        <MenuIcon size={26} className="active:scale-90 transition" />
+                    </button>
                 </div>
-
-                <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
-                    Start free trial
-                </button>
-                <button onClick={() => setIsOpen(true)} className="md:hidden">
-                    <MenuIcon size={26} className="active:scale-90 transition" />
-                </button>
             </motion.nav>
 
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                {navlinks.map((link: INavLink) => (
-                    <NavLink key={link.name} to={link.href} onClick={() => setIsOpen(false)}>
-                        {link.name}
-                    </NavLink>
-                ))}
-                <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
+                <Link onClick={() => setIsOpen(false)} to='/'>Home</Link>
+                <Link onClick={() => setIsOpen(false)} to='/generate'>Generate</Link>
+                <Link onClick={() => setIsOpen(false)} to='/my-generation'>My Generations</Link>
+                <Link onClick={() => setIsOpen(false)} to='#'>Contact</Link>
+                <Link onClick={() => setIsOpen(false)} to='/login'>Login</Link>
+                <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex" aria-label="Close menu" title="Close menu">
                     <XIcon />
                 </button>
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.js'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
